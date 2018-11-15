d8571967f20f624b6c70f570f4e4b9a7256e8790
zeljko todorovic <zeljkotodorovic970@gmail.com>
2018-11-15 10:14:57 +0100

    remove @Transactional

--- src/main/java/com/zeljko/students2/service/CourseServiceImpl.java
+++ src/main/java/com/zeljko/students2/service/CourseServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
+//@Transactional
 public class CourseServiceImpl implements CourseService {
 
     private CourseRepository courseRepository;
